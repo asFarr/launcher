3634dfa7b9135c6620d07135c061e5f3184d83c9
Alex Farr <47061989+asFarr@users.noreply.github.com>
2022-07-12 20:10:40 -0400

    Begin v3 work.
    Added basic YAML config parsing to launcher.py:
     - redirected variables to pull from yaml ingest
     - began building /config directory and files
    
    Updated README.md to reflect push to v3:
     - began scoping intended changes in v3 changelog and TODO

--- README.md
+++ README.md
@@ -1,11 +1,90 @@
-# The CerealBox toolkit: Window Launcher (v2.0)
-Simple window control tool using flag and argument parsing for command line automation. 
 
-This tool is constrained to a windows environment due to it's usage of win32gui. 
+# The Cerealbox toolkit: Window Launcher v3.0 
+by: Alex Farr\
+`Release Dates: v1: 01/18/21, v2: 04/28/22, v3: 07/12/22`
 
-It is also currently hardcoded to configure a selection of windows that I used for testing the concept. 
+---
+### v3 Changelog
 
-I will be starting my third pass over this project soon, one of the features I intend to add is a yaml file for settings configuration before runtime.
+- YAML Config files have been added under /config, and parsing has been added 
+    to the beginning of the script in a basic implementation. Needs to be expanded to
+    allow for remote and local dimension loading once added to config schema.
+- -t and -m have been replaced by -l and -r in the flag parser. 
+    
+#### TODO:
+- Add full named flags in addition to single letter flags.
+- -l and -r need to be fully documented, and new control logic needs to be built. 
 
-The abstractions required over my current solution will more or less require another entire redesign of the project to implement, 
-but once completed it should make the tool usable by others without the significant modification of the main script that is currently required. 
+---
+
+### v2 Documentation
+
+An automation-ready command line tool for launching and organizing windows into predefined
+configurations to optimize workflow.
+
+This tool is constrained to a Windows environment due to its usage of win32gui. 
+
+It is also currently hardcoded to configure a selection of windows that I used for testing
+the concept.
+
+```
+    Usage: python launcher.py {-a, -h} {-t|-m}
+        Default flags are -a and -h.
+
+    Examples:
+    launcher.py -a    -   Display the author tag.
+    launcher.py -h    -   Display the help dialog.
+    launcher.py -t    -   Runs 'tall' window configuration settings
+    launcher.py -m    -   Runs 'mid' window configuration settings
+```
+
+---
+
+### v1 Documentation
+
+This script is built specifically for my personal desktop, and is designed to open and organize
+applications that I always want in the same place. Due to this fact, it employs constants which
+correspond to the relative window positions required on my specific resolution and configuration.
+
+Other dependencies than those imported here are required for execution of this script.
+When installing dependencies through pip, follow this order for optimal results:
+opencv-python, Pillow, pyautogui, pywin32, psutil
+
+'psutil' is used to check the process tree for an instance of the application and stores the result
+in the boolean 'running'. If the boolean is False, then os.startfile is used to call the binaries
+from the qualified paths stored in the associated constants, where 'time' is used to delay further
+script execution based on predefined constants to allow for the processes to load fully.
+
+```python
+DSCRD_DELAY = int(sec)  # used to control how long script execution pauses while loading Discord.
+STEAM_DELAY = int(sec)  # used to control how long script execution pauses while loading Steam.
+```
+
+```python
+DSCRD_PATH = r'__path__'  # fully qualified filepath for Discord binaries, usually in AppData Local.
+STEAM_PATH = r'__path__'  # fully qualified filepath for Steam binaries, usually in ProgramFiles.
+```
+
+'win32gui' is used to grab the COM Object for the window handler associated with each application,
+based on their window title information. Once each handler is found, it is manipulated according
+to the dimensions specified in the following constant arrays.
+
+```python
+DI_DIM = [x_loc, y_loc, width, height]  # location and dimensions for Discord chat.
+ST_DIM = [x_loc, y_loc, width, height]  # location and dimensions for Steam Games list.
+SF_DIM = [x_loc, y_loc, width, height]  # location and dimensions for Steam Friends list.
+```
+
+'pyautogui' is used if the COM Object for Steam Friends List cannot be found. It references the
+contents of `/imgsrc` in order to locate and navigate through the top menu of the Steam Games list
+to open the Friends List according to an image-matching confidence value defined in CONF.
+
+`CONF = float(0-1)` -- used to control the accuracy restrictions on the image recognition phase.
+Lower than .4 appears to regularly produce false-positives, and higher than `.6 - .7` seems
+unable to locate references in screen space.
+
+Also, on a final note, for the script not to fail in various places the following two configs
+must be met:
+
+`Steam -> View -> Small Mode` (might make this an image recognition check at some point)
+Discord must be on friends list, no specific user or server chat may be pulled up.
--- launcher.py
+++ launcher.py
@@ -1,73 +1,122 @@
 """
-The CerealBox toolkit: Window Launcher (v2.0) - by asFarr : 4/28/22
-
-An automation-ready command line tool for organizing windows
-into predefined configurations to optimize workflow.
-
-    Usage: python launcher.py {-a, -h} {-t|-m}
-    Default flags are -a and -h.
-
-    Examples:
-    launcher.py -a    -   Display the author tag.
-    launcher.py -h    -   Display the help dialog.
-    launcher.py -t    -   Runs 'tall' window configuration settings
-    launcher.py -m    -   Runs 'mid' window configuration settings
+Launcher v3.0 by Alex Farr
+Written: 07/12/22
 """
 
-import webbrowser as web
-import win32gui as gui
-import psutil
-import getopt
-import time
-import sys
-import os
 import re
+import os
+import sys
+import time
+import getopt
+import psutil as ps
+import pyautogui as ag
+import win32gui as gui
+import webbrowser as web
+import yaml
+try:
+    from yaml import load, CSafeLoader as SafeLoader
 
-APPS = ['OUTLOOK', 'Spotify', 'Teams', 'Brave']
+except ImportError:
+    from yaml import load, SafeLoader
 
-SPOT_PATH = r'C:\Users\asfarr\AppData\Roaming\Spotify\Spotify.exe'
-OUTL_PATH = r'C:\Program Files (x86)\Microsoft Office\Office16\OUTLOOK.EXE'
+# Script initialization
 
-tm_dim = [2561, 0, 853, 1400]
-ol_dim = [3413, 0, 853, 1400]
-sp_dim = [4260, 0, 865, 1400]
-br_dim = [0, -1080, 1920, 1080]
-
-APP_DELAY = 8
+ag.PAUSE = 0.25
+CONF = .5
 re.IGNORECASE = True
 
-url = r''
+# Load specified configuration files
+
+with open("config/brave_config.yaml", mode="rt", encoding="utf-8") as config0:
+    brave_cfg = yaml.safe_load(config0)
+
+BRAVE_DELAY = brave_cfg["load-delay"]
+BRAVE_PATH = brave_cfg["application"]["path"]
+
+br_dim = []
+for val in brave_cfg["dimensions_games"].values():
+    br_dim.append(val)
+
+with open("config/discord_config.yaml", mode="rt", encoding="utf-8") as config1:
+    dscrd_cfg = yaml.safe_load(config1)
+
+DSCRD_DELAY = dscrd_cfg["load-delay"]
+DSCRD_PATH = dscrd_cfg["application"]["path"]
+
+di_dim = []
+for val in dscrd_cfg["dimensions_games"].values():
+    di_dim.append(val)
+
+with open("config/obsidian_config.yaml", mode="rt", encoding="utf-8") as config2:
+    obsd_cfg = yaml.safe_load(config2)
+
+OBSD_DELAY = obsd_cfg["load-delay"]
+OBSD_PATH = obsd_cfg["application"]["path"]
+
+ob_dim = []
+for val in obsd_cfg["dimensions_games"].values():
+    ob_dim.append(val)
+
+with open("config/spotify_config.yaml", mode="rt", encoding="utf-8") as config3:
+    spot_cfg = yaml.safe_load(config3)
+
+SPOT_DELAY = spot_cfg["load-delay"]
+SPOT_PATH = spot_cfg["application"]["path"]
+
+sp_dim = []
+for val in spot_cfg["dimensions_games"].values():
+    sp_dim.append(val)
+
+with open("config/steam_config.yaml", mode="rt", encoding="utf-8") as config4:
+    steam_cfg = yaml.safe_load(config4)
+
+STEAM_DELAY = steam_cfg["load-delay"]
+STEAM_PATH = steam_cfg["application"]["path"]
+
+sg_dim = []
+for val in steam_cfg["dimensions_games"].values():
+    sg_dim.append(val)
+sf_dim = []
+for val in steam_cfg["dimensions_friends"].values():
+    sf_dim.append(val)
+
+
+app_list = [dscrd_cfg["application"]["name"],
+            brave_cfg["application"]["name"],
+            obsd_cfg["application"]["name"],
+            spot_cfg["application"]["name"],
+            steam_cfg["application"]["name"]]
 
 
 def author():
     """Print the author ASCII signature."""
     authorart = [
-"                                     88         888888888888                               88             d8'",
-"                                     88              88                                    88            d8'",
-"                                     88              88                                    88           \"\"",
-"                                     88   ,adPPYba,  88   ,adPPYba,   ,adPPYYba,   ,adPPYb,88  ,adPPYba,",
-"                                     88  a8P_____88  88  a8\"     \"8a  \"\"     `Y8  a8\"    `Y88  I8[    \"\"",
-"                                     88  8PP\"\"\"\"\"\"\"  88  8b       d8  ,adPPPPP88  8b       88   `\"Y8ba,",
-"                                     88  \"8b,   ,aa  88  \"8a,   ,a8\"  88,    ,88  \"8a,   ,d88  aa    ]8I",
-"                                     88   `\"Ybbd8\"'  88   `\"YbbdP\"'   `\"8bbdP\"Y8   `\"8bbdP\"Y8  `\"YbbdP\"'\n\n\n",
-"                     ,ad8888ba,                                                   88  88888888ba",
-"                    d8\"'    `\"8b                                                  88  88      \"8b",
-"                   d8'                                                            88  88      ,8P",
-"                   88              ,adPPYba,  8b,dPPYba,   ,adPPYba,  ,adPPYYba,  88  88aaaaaa8P'   ,adPPYba,  8b,     ,d8",
-"                   88             a8P_____88  88P'   \"Y8  a8P_____88  \"\"     `Y8  88  88\"\"\"\"\"\"8b,  a8\"     \"8a  `Y8, ,8P'",
-"                   Y8,            8PP\"\"\"\"\"\"\"  88          8PP\"\"\"\"\"\"\"  ,adPPPPP88  88  88      `8b  8b       d8    )888(",
-"                    Y8a.    .a8P  \"8b,   ,aa  88          \"8b,   ,aa  88,    ,88  88  88      a8P  \"8a,   ,a8\"  ,d8\" \"8b,",
-"                     `\"Y8888Y\"'    `\"Ybbd8\"'  88           `\"Ybbd8\"'  `\"8bbdP\"Y8  88  88888888P\"    `\"YbbdP\"'  8P'     `Y8\n\n\n",
-"       db                                      88                                                             88",
-"      d88b                                     88                                                             88",
-"     d8'`8b                                    88                                                             88",
-"    d8'  `8b      8b,dPPYba,   8b,dPPYba,      88           ,adPPYYba,  88       88  8b,dPPYba,    ,adPPYba,  88,dPPYba,    ,adPPYba,  8b,dPPYba,",
-"   d8YaaaaY8b     88P'    \"8a  88P'    \"8a     88           \"\"     `Y8  88       88  88P'   `\"8a  a8\"     \"\"  88P'    \"8a  a8P_____88  88P'   \"Y8",
-"  d8\"\"\"\"\"\"\"\"8b    88       d8  88       d8     88           ,adPPPPP88  88       88  88       88  8b          88       88  8PP\"\"\"\"\"\"\"  88",
-" d8'        `8b   88b,   ,a8\"  88b,   ,a8\"     88           88,    ,88  \"8a,   ,a88  88       88  \"8a,   ,aa  88       88  \"8b,   ,aa  88",
-"d8'          `8b  88`YbbdP\"'   88`YbbdP\"'      88888888888  `\"8bbdP\"Y8   `\"YbbdP'Y8  88       88   `\"Ybbd8\"'  88       88   `\"Ybbd8\"'  88",
-"                  88           88",
-"                  88           88\n"]
+        "                                     88         888888888888                               88             d8'",
+        "                                     88              88                                    88            d8'",
+        "                                     88              88                                    88           \"\"",
+        "                                     88   ,adPPYba,  88   ,adPPYba,   ,adPPYYba,   ,adPPYb,88  ,adPPYba,",
+        "                                     88  a8P_____88  88  a8\"     \"8a  \"\"     `Y8  a8\"    `Y88  I8[    \"\"",
+        "                                     88  8PP\"\"\"\"\"\"\"  88  8b       d8  ,adPPPPP88  8b       88   `\"Y8ba,",
+        "                                     88  \"8b,   ,aa  88  \"8a,   ,a8\"  88,    ,88  \"8a,   ,d88  aa    ]8I",
+        "                                     88   `\"Ybbd8\"'  88   `\"YbbdP\"'   `\"8bbdP\"Y8   `\"8bbdP\"Y8  `\"YbbdP\"'\n\n\n",
+        "                     ,ad8888ba,                                                   88  88888888ba",
+        "                    d8\"'    `\"8b                                                  88  88      \"8b",
+        "                   d8'                                                            88  88      ,8P",
+        "                   88              ,adPPYba,  8b,dPPYba,   ,adPPYba,  ,adPPYYba,  88  88aaaaaa8P'   ,adPPYba,  8b,     ,d8",
+        "                   88             a8P_____88  88P'   \"Y8  a8P_____88  \"\"     `Y8  88  88\"\"\"\"\"\"8b,  a8\"     \"8a  `Y8, ,8P'",
+        "                   Y8,            8PP\"\"\"\"\"\"\"  88          8PP\"\"\"\"\"\"\"  ,adPPPPP88  88  88      `8b  8b       d8    )888(",
+        "                    Y8a.    .a8P  \"8b,   ,aa  88          \"8b,   ,aa  88,    ,88  88  88      a8P  \"8a,   ,a8\"  ,d8\" \"8b,",
+        "                     `\"Y8888Y\"'    `\"Ybbd8\"'  88           `\"Ybbd8\"'  `\"8bbdP\"Y8  88  88888888P\"    `\"YbbdP\"'  8P'     `Y8\n\n\n",
+        "       db                                      88                                                             88",
+        "      d88b                                     88                                                             88",
+        "     d8'`8b                                    88                                                             88",
+        "    d8'  `8b      8b,dPPYba,   8b,dPPYba,      88           ,adPPYYba,  88       88  8b,dPPYba,    ,adPPYba,  88,dPPYba,    ,adPPYba,  8b,dPPYba,",
+        "   d8YaaaaY8b     88P'    \"8a  88P'    \"8a     88           \"\"     `Y8  88       88  88P'   `\"8a  a8\"     \"\"  88P'    \"8a  a8P_____88  88P'   \"Y8",
+        "  d8\"\"\"\"\"\"\"\"8b    88       d8  88       d8     88           ,adPPPPP88  88       88  88       88  8b          88       88  8PP\"\"\"\"\"\"\"  88",
+        " d8'        `8b   88b,   ,a8\"  88b,   ,a8\"     88           88,    ,88  \"8a,   ,a88  88       88  \"8a,   ,aa  88       88  \"8b,   ,aa  88",
+        "d8'          `8b  88`YbbdP\"'   88`YbbdP\"'      88888888888  `\"8bbdP\"Y8   `\"YbbdP'Y8  88       88   `\"Ybbd8\"'  88       88   `\"Ybbd8\"'  88",
+        "                  88           88",
+        "                  88           88\n"]
 
     for i in authorart:
         print(i)
@@ -75,37 +124,97 @@ def author():
 
 def helpme():
     """Print the help and usage dialog."""
-    usage = ["The CerealBox toolkit: Window Launcher (v2.0) - by asFarr : 4/28/22\n",
+    usage = ["\nThe CerealBox toolkit: Window Launcher (v3.0) - by asFarr : 7/12/22\n",
+             "An automation-ready command line tool for organizing windows",
+             "into predefined configurations to optimize workflow.\n",
 
-"An automation-ready command line tool for organizing windows",
-"into predefined configurations to optimize workflow.\n",
+             "    Usage: python launcher.py {-a, -h} {-g|-s|-w} {-l|-r}",
+             "    Default flags are -a and -h.\n",
 
-"    Usage: python launcher.py {-a, -h} {-t|-m}",
-"    Default flags are -a and -h.\n",
+             "    Examples:\n",
 
-"    Examples:",
-"    launcher.py -a    -   Display the author tag.",
-"    launcher.py -h    -   Display the help dialog.",
-"    launcher.py -t    -   Runs 'tall' window configuration settings",
-"    launcher.py -m    -   Runs 'mid' window configuration settings"]
+             "Information Flags:",
+             "    launcher -a    -   Display the author tag.",
+             "    launcher -h    -   Display the help dialog.\n",
+
+             "Application Flags",
+             "    launcher -g    -   Run Gaming Mode.",
+             "    launcher -s    -   Run Server Dashboard Mode.",
+             "    launcher -w    -   Run Workstation Mode.\n",
+
+             "Screen Context Flags",
+             "    launcher -l    -   Use Local Screen Context.",
+             "    launcher -r    -   Use Remote Screen Context."]
 
     for i in usage:
         print(i)
 
 
-def set_dimensions(hwnd, extra):
-    """Configure window dimensions for predefined set of window frames"""
-    if re.search(APPS[0], gui.GetWindowText(hwnd)):
-        gui.MoveWindow(hwnd, ol_dim[0], ol_dim[1], ol_dim[2], ol_dim[3], True)
+def appstart():
+    for app in app_list:
+        running = app + '.exe' in (i.name() for i in ps.process_iter())
+        if not running:
+            if re.search(app_list[0], app):
+                os.startfile(OBSD_PATH)
+                time.sleep(OBSD_DELAY)
+            if re.search(app_list[1], app):
+                os.startfile(SPOT_PATH)
+                time.sleep(SPOT_DELAY)
+            if re.search(app_list[2], app):
+                os.startfile(DSCRD_PATH)
+                time.sleep(DSCRD_DELAY)
+            if re.search(app_list[3], app):
+                os.startfile(STEAM_PATH)
+                time.sleep(STEAM_DELAY)
+            if re.search(app_list[4], app):
+                os.startfile(BRAVE_PATH)
+                time.sleep(BRAVE_DELAY)
 
-    if re.search(APPS[1], gui.GetWindowText(hwnd)):
-        gui.MoveWindow(hwnd, sp_dim[0], sp_dim[1], sp_dim[2], sp_dim[3], True)
 
-    if re.search(APPS[2], gui.GetWindowText(hwnd)):
-        gui.MoveWindow(hwnd, tm_dim[0], tm_dim[1], tm_dim[2], tm_dim[3], True)
+def discord():
+    """Check to see if Discord is running, open it if not, then resize and move it. """
+    running = "Discord.exe" in (i.name() for i in ps.process_iter())
+    if not running:
+        os.startfile(DSCRD_PATH)
+        time.sleep(DSCRD_DELAY)
+    hwnd = gui.FindWindow(None, app_list[0])
+    gui.MoveWindow(hwnd, di_dim[0], di_dim[1], di_dim[2], di_dim[3], True)
 
-    if re.search(APPS[3], gui.GetWindowText(hwnd)):
-        gui.MoveWindow(hwnd, br_dim[0], br_dim[1], br_dim[2], br_dim[3], True)
+
+def steam():
+    """
+    Check to see if Steam is running, open it if not, then resize and move it.
+    Check if Friends List is open, open it if not, using image recognition on menus.
+    """
+
+    running = "steam.exe" in (i.name() for i in ps.process_iter())
+    if not running:
+        os.startfile(STEAM_PATH)
+        time.sleep(STEAM_DELAY)
+
+    steam_window = gui.FindWindow(None, app_list[4])
+    gui.MoveWindow(steam_window, sg_dim[0], sg_dim[1], sg_dim[2], sg_dim[3], True)
+
+    if gui.FindWindow(None, "Friends List") == 0:
+        x_loc, y_loc, width, height = ag.locateOnScreen(
+            r'imgsrc\steammenus.png', grayscale=True, confidence=CONF)
+        x_loc, y_loc = ag.locateCenterOnScreen(
+            r'imgsrc\friends.png', grayscale=True,
+            region=(x_loc, y_loc, width, height), confidence=CONF)
+        ag.moveTo(x_loc, y_loc)
+        ag.click(clicks=1, interval=0, button='left')
+        x_loc, y_loc, width, height = ag.locateOnScreen(
+            r'imgsrc\friendsmenu.png', grayscale=True, confidence=CONF)
+        x_loc, y_loc = ag.locateCenterOnScreen(
+            r'imgsrc\friendslist.png', grayscale=True,
+            region=(x_loc, y_loc, width, height), confidence=CONF)
+        ag.moveTo(x_loc, y_loc)
+        ag.click(clicks=1, interval=0, button='left')
+        friends_window = gui.FindWindow(None, "Friends List")
+        gui.MoveWindow(friends_window, sf_dim[0], sf_dim[1], sf_dim[2], sf_dim[3], True)
+    else:
+        friends_window = gui.FindWindow(None, "Friends List")
+        gui.MoveWindow(friends_window, sf_dim[0], sf_dim[1], sf_dim[2], sf_dim[3], True)
 
 
 def main(args):
@@ -115,46 +224,48 @@ def main(args):
     else:  # inherit flags/args list
         argument_list = args
 
-    options = "ahtm"  # define range of possible flags
-    for app in APPS:
-        running = app + '.exe' in (i.name() for i in psutil.process_iter())
-        if not running:
-            if re.search(APPS[1], app):
-                os.startfile(SPOT_PATH)
-                time.sleep(APP_DELAY)
-            if re.search(APPS[0], app):
-                os.startfile(OUTL_PATH)
-            if re.search(APPS[3], app):
-                web.open_new(url)
+    options = "ahrlgws"  # define range of possible flags
 
     try:  # attempt to map flags to their corresponding arguments
         arguments, values = getopt.getopt(argument_list, options)
 
         # logical matching for arguments to driving logic
         for current_argument, current_value in arguments:
-            if current_argument == "-a":
+            if current_argument == "-a":  # -Author Dialog
                 author()
 
-            if current_argument == "-h":
+            if current_argument == "-h":  # -Help Dialog
                 helpme()
 
-            if current_argument == "-t":
-                gui.EnumWindows(set_dimensions, None)
+            if current_argument == "-r":  # -Remote orientation Mode
+                print("remote flag")
+                print("needs to write window dimension values for remote window orientation to dim variables")
+                print("then needs to run dim setting logic, or call function that does")
 
-            if current_argument == "-m":
-                ol_dim[1] = ol_dim[1] + 700
-                ol_dim[3] = int(ol_dim[3] / 2)
-                sp_dim[1] = sp_dim[1] + 700
-                sp_dim[3] = int(sp_dim[3] / 2)
-                tm_dim[1] = tm_dim[1] + 700
-                tm_dim[3] = int(tm_dim[3] / 2)
-                gui.EnumWindows(set_dimensions, None)
+            if current_argument == "-l":  # -Local orientation Mode
+                print("local flag")
+                print("needs to write window dimension values for local window orientation to dim variables")
+                print("then needs to run dim setting logic, or call function that does")
+
+            if current_argument == "-g":  # -Gaming app launch Mode
+                print("gaming flag")
+                print("needs to write gaming app names and filepaths to name-path dictionary")
+                print("then needs to call launch check method to iterate through dictionary and launch apps")
+
+            if current_argument == "-w":  # -Workstation app launch Mode
+                print("workstation flag")
+                print("needs to write coding/development and documenation "
+                      "app names and filepaths to name-path dictionary")
+                print("then needs to call launch check method to iterate through dictionary and launch apps")
+
+            if current_argument == "-s":  # -Server (Homelab) app launch Mode
+                print("homelab flag")
+                print("needs to write homelab site URLs and browser filepath to name-path dictionary")
+                print("then needs to call launch check method to iterate through dictionary and launch apps")
 
     except getopt.error as err:  # if mapping fails, pass stderr through to output
         print(str(err))
 
 
 if __name__ == '__main__':
-    # pass main driver function args list from sys
-    # start at index 1 to skip over the command call itself
     main(sys.argv[1:])
